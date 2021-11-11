e86d7df5e6030df4c224dbfa4ba66b0362242fd4
MJSasha <sasha.ushankov@gmail.com>
2021-11-11 16:25:55 +0300

    Add order check

--- Test/Models/Order.cs
+++ Test/Models/Order.cs
@@ -11,20 +11,30 @@ namespace Test.Models
         public int Id { get; set; }
 
         [Display(Name ="Введите Имя")]
+        [MinLength(1)]
+        [Required(ErrorMessage ="Заполните поле")]
         public string Name { get; set; }
 
         [Display(Name = "Введите Фамилию")]
+        [MinLength(1)]
+        [Required(ErrorMessage = "Заполните поле")]
         public string LastName { get; set; }
 
         [Display(Name = "Введите Адрес")]
+        [MinLength(1)]
+        [Required(ErrorMessage = "Заполните поле")]
         public string Adres { get; set; }
 
         [Display(Name = "Введите Телефон")]
         [DataType(DataType.PhoneNumber)]
+        [MinLength(9)]
+        [Required(ErrorMessage = "Заполните поле")]
         public string Phone { get; set; }
 
         [Display(Name = "Введите Почту")]
         [DataType(DataType.EmailAddress)]
+        [MinLength(4)]
+        [Required(ErrorMessage = "Заполните поле")]
         public string Email { get; set; }
 
         [BindNever]
